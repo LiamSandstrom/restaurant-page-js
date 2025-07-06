c59b1975f7e002cf87f94bbe7efd288bd90fae09
LiamSandstrom <113420205+LiamSandstrom@users.noreply.github.com>
2025-07-06 15:39:27 +0200

    test

--- src/index.js
+++ src/index.js
@@ -51,5 +51,7 @@ class pageHandler {
     myPageHandler.changePage("booking");
   });
 
-  console.log("test")
+  menuBtn.addEventListener("click", () => {
+    myPageHandler.changePage("menu");
+  });
 })();
